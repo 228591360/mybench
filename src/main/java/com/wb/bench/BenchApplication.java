9ac5f1d11b3629a14f633ecd4742ee0350ed8970
wangbiao <wangbiao@vandream>
2021-04-14 11:55:12 +0800

    mybatisPlus生成器

--- src/main/java/com/wb/bench/BenchApplication.java
+++ src/main/java/com/wb/bench/BenchApplication.java
@@ -3,10 +3,12 @@ package com.wb.bench;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class BenchApplication {
     public static void main(String[] args) {
         SpringApplication.run(BenchApplication.class, args);
+        System.out.println("spring start..........");
     }
-
 }
