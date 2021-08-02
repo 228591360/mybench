c318ed26b9ba70b37cfa7ef06822c84daabbeb9b
wangbiao <wangbiao@vandream>
2021-08-02 15:25:41 +0800

    笔记

--- src/main/java/com/wb/bench/service/Impl/UserServerImpl.java
+++ src/main/java/com/wb/bench/service/Impl/UserServerImpl.java
@@ -1,14 +1,18 @@
 package com.wb.bench.service.Impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wb.bench.entity.User;
 import com.wb.bench.mapper.UserMapper;
 import com.wb.bench.service.UserServer;
 import com.wb.bench.util.RedisUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: WangBiao
@@ -41,5 +45,16 @@ public class UserServerImpl extends ServiceImpl<UserMapper, User> implements Use
     @Override
     public int createUser(User user) {
         return userMapper.insert(user);
+    }
+
+    public static void main(String[] args) {
+        Map userInfoMap = new HashMap();
+        userInfoMap.put("age","26");
+        userInfoMap.put("name","用户名XXX");
+        User user = new User();
+        User userParse = JSON.parseObject(JSON.toJSONString(userInfoMap), User.class);
+        BeanUtils.copyProperties(userParse,user);
+        System.out.println(user);
+
     }
 }
