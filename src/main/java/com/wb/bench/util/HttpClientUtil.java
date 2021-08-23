25e1b7addde4b25762d5ce13efa8c2effd005505
wangbiao <wangbiao@vandream>
2021-08-23 17:47:00 +0800

    工具类

---
+++ src/main/java/com/wb/bench/util/HttpClientUtil.java
@@ -0,0 +1,183 @@
+package com.wb.bench.util;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * httpclientutils
+ * @author wangxiaolei
+ * @date 2020/5/7 18:36
+ */
+public class HttpClientUtil {
+
+    private static Logger log = LoggerFactory.getLogger(HttpClientUtil.class);
+    private HttpClientUtil() {
+    }
+
+    private static RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectTimeout(5000).setConnectionRequestTimeout(1000)
+            .setSocketTimeout(60000).build();
+
+    public static String doGet(String url, Map<String, String> param) {
+
+        // 创建Httpclient对象
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        String resultString = null;
+        CloseableHttpResponse response = null;
+        try {
+            // 创建uri
+            URIBuilder builder = new URIBuilder(url);
+            if (param != null) {
+                for (String key : param.keySet()) {
+                    builder.addParameter(key, param.get(key));
+                }
+            }
+            URI uri = builder.build();
+
+            // 创建http GET请求
+            HttpGet httpGet = new HttpGet(uri);
+            httpGet.setConfig(requestConfig);
+            // 执行请求
+            response = httpclient.execute(httpGet);
+            // 判断返回状态是否为200
+            if (response.getStatusLine().getStatusCode() == 200) {
+                resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage() ,e);
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+                httpclient.close();
+            } catch (IOException e) {
+                log.error(e.getMessage(),e);
+            }
+        }
+        return resultString;
+    }
+
+    public static String doGet(String url) {
+        return doGet(url, null);
+    }
+
+    public static String doPost(String url, Map<String, Object> param) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setConfig(requestConfig);
+            // 创建参数列表
+            if (param != null) {
+                List<NameValuePair> paramList = new ArrayList<>();
+                for (String key : param.keySet()) {
+                    paramList.add(new BasicNameValuePair(key, (String) param.get(key)));
+                }
+                // 模拟表单
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList);
+                httpPost.setEntity(entity);
+            }
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        } catch (Exception e) {
+            log.error(e.getMessage(),e);
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+                httpClient.close();
+            } catch (IOException e) {
+                log.error(e.getMessage(),e);
+            }
+        }
+
+        return resultString;
+    }
+
+    public static String doPost(String url) {
+        return doPost(url, null);
+    }
+
+    public static String doPostJson(String url, String json,String token_header) throws Exception {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setConfig(requestConfig);
+            // 创建请求内容
+            httpPost.setHeader("HTTP Method","POST");
+            httpPost.setHeader("Connection","Keep-Alive");
+            httpPost.setHeader("Content-Type","application/json;charset=utf-8");
+            if(StringUtils.isNotEmpty(token_header)) {
+                httpPost.setHeader("x-authentication-token", token_header);
+            }
+
+            StringEntity entity = new StringEntity(json);
+
+            entity.setContentType("application/json;charset=utf-8");
+            httpPost.setEntity(entity);
+
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(),e);
+            throw e;
+        } finally {
+            try {
+                if(response!=null){
+                    response.close();
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage(),e);
+            }
+        }
+
+        return resultString;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://antgine.paybay.cn/activity/excode/createByActivity";
+        Map map = new HashMap<>();
+        map.put("ActID","1629706160948703119");
+        map.put("BuyerID","qwertyuiop123");
+        map.put("Token","01CZFCJ0XKSMGE8MD1156DBYKZ");
+        String jsonString = JSON.toJSONString(map);
+        String s = doPostJson(url, jsonString,null);
+        System.out.println(s);
+    }
+}
